ab91ee434946a3db83275e2c8b889f5f6e978062
NoemiMassucco <noemimassucco@gmail.com>
2025-12-25 23:01:41 +0100

    Eseguito conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,26 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa
+ e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(word) {
+    const vocali = 'aeiou';
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        if (vocali.includes(word[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}   
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const risultato = contaVocali(word);
+console.log(risultato); 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente 
+un nome e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -6,7 +7,6 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
 
 // Dichiara la funzione qui.
 
