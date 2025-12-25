94ca0bb7ea54486bea53c41d64afad0b50844754
NoemiMassucco <noemimassucco@gmail.com>
2025-12-25 23:03:12 +0100

    Esercizio bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,26 @@ buongiorno se è mattina (fino alle 13),
 buon pomeriggio se è pomeriggio (fino alle 17) 
 e buonasera se è sera (oltre le 17)
 */
-
 const name = 'Mario';
 
 // Dichiara la funzione qui.
+function saluta(name) {
+    const ora = new Date().getHours();
+    let saluto;
+
+    if (ora < 13) {
+        saluto = 'Buongiorno';
+    } else if (ora < 18) {
+        saluto = 'Buon pomeriggio';
+    } else {
+        saluto = 'Buonasera';
+    }
+
+    return saluto + ' ' + name + '.';
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluta(name));              
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
