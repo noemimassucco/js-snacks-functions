0aa9f4b4f3a0e74a3adcf11c3d3f98fbe5082687
NoemiMassucco <noemimassucco@gmail.com>
2025-12-25 22:43:30 +0100

    1 ESERCIZIO SALUTA FATTO

---
+++ 01_saluti/script.js
@@ -0,0 +1,18 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function saluta() {
+
+
+
+
+// Invoca la funzione qui e stampa il risultato in console
+    return 'Ciao ' + userName;
+}
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(saluta());
